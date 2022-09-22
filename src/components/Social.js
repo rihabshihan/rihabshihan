b7118166b194b57590f8484c4936c15e5c1f235d
rihabshihan <114134905+rihabshihan@users.noreply.github.com>
2022-09-22 14:43:17 +0530

    Add files via upload

---
+++ src/components/Social.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://codepen.io/S rihab" target="_blank" rel="noopener noreferrer"><i class="fab fa-codepen"></i></a>
+            <a href="https://github.com/rihabshihan" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://Instagram.com/rihab_shihan" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/Rihab Shihan" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in/Rihab Shihan" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@rihab" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
+    
